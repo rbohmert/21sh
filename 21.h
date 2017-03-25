c0a3192279dc25d33563b36043415ccfbf8ce6ba
Ruben BOHMERT <rbohmert@e1r13p13.42.fr>
2017-03-25 10:05:49 +0100

    fe

---
+++ 21.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   21.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbohmert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/02 04:33:53 by rbohmert          #+#    #+#             */
+/*   Updated: 2017/03/25 04:16:19 by rbohmert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEUXUN_H
+# define DEUXUN_H
+
+# include "libft/libft.h"
+# include "minishell/minishell.h"
+# define SYM 0b00000001
+# define SEP 0b00000010
+# define PIP 0b00000100
+# define RED 0b00001000
+# define RFD 0b00010000
+# define CISSYM(c) (c == '<' || c == '>' || c == ';' || c == '|' || c == '&')
+# define TYPE content_size
+# define ISSYM(x) (x & SYM)
+# define ISSEP(x) (x & SEP)
+# define ISPIP(x) (x & PIP)
+# define ISRED(x) (x & RED)
+# define ISRFD(x) (x & RFD)
+# define L(x) ((t_item *)(x->content))
+# define T(x) ((t_item *)(x->content))
+
+typedef	struct	s_item
+{
+	void		*itm;
+	int			type;
+}				t_item;
+
+t_list *lexer(char *line);
+t_tree *parser(t_list *beg);
+t_tree *make_tree(t_list *beg, t_list *end, int flag);
+t_list *lstchrend(t_list *lst, int flag);
+int		res(t_tree *tree, t_list *in, t_list *out);
+char 	*lsttostr(t_list *lst);
+char **sg_env(char **env);
+int		manage_in(t_list *in);
+int		manage_out(int outcom, t_list *out);
+
+#endif
