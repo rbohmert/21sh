cbe425de6a097477f76776562f716c9f9bad09f9
user <user@debian>
2017-04-19 12:02:37 +0200

    commentaire et tout

---
+++ minishell/env.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbohmert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/20 22:02:48 by rbohmert          #+#    #+#             */
+/*   Updated: 2017/03/11 01:37:35 by rbohmert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**cp_env(char **env)
+{
+	int		i;
+	char	**nenv;
+
+	i = 0;
+	while (env[i])
+		i++;
+	nenv = (char **)malloc((i + 10) * sizeof(char *));
+	i = 0;
+	while (env[i])
+	{
+		nenv[i] = env[i];
+		i++;
+	}
+	nenv[i] = NULL;
+	return (nenv);
+}
+
+void	clr_env(char **nenv)
+{
+	int i;
+
+	i = 0;
+	while (nenv[i])
+	{
+		i++;
+		nenv[i - 1] = NULL;
+	}
+}
+
+void	add_key(char *str, char **nenv)
+{
+	int i;
+
+	i = 0;
+	while (nenv[i])
+		i++;
+	nenv[i] = ft_strdup(str);
+	nenv[i + 1] = NULL;
+}
+
+void	env_opt(char **arg, char **nenv, int *i)
+{
+	while (arg[*i] && arg[*i][0] == '-')
+	{
+		if (arg[*i][1] == 'i' && arg[*i][2] == 0)
+			clr_env(nenv);
+		else if (arg[*i][1] == 'u' && arg[*i][2] == 0 && arg[*i + 1])
+			ft_unsetenv(arg + (*i)++, nenv);
+		else
+			ft_putstr("usage env [-i] [-u name] [utility [arguments ...]]\n");
+		(*i)++;
+	}
+	while (arg[*i] && ft_strchr(arg[*i], '='))
+		add_key(arg[(*i)++], nenv);
+}
+
+void	ft_env(char **arg, char **env)
+{
+	int		i;
+	char	**nenv;
+	char	*com;
+
+	i = 1;
+	nenv = cp_env(env);
+	env_opt(arg, nenv, &i);
+	if (arg[i] && (com = check(env, arg[i], 1)))
+		exe_com(com, arg + i, nenv, NULL, NULL);
+	else if (!arg[i])
+		ft_ptabstr(nenv);
+	else
+	{
+		ft_putstr("env: ");
+		ft_putstr(arg[i]);
+		ft_putstr(": commande introuvable\n");
+	}
+	free(nenv);
+}
---
+++ minishell/minishell.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbohmert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/20 19:09:49 by rbohmert          #+#    #+#             */
+/*   Updated: 2017/03/31 10:56:07 by rbohmert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+
+char	*check(char **env, char *name, int no_built)
+{
+	int			i;
+	char		*path;
+	char		**dir;
+	struct stat	buf;
+
+	i = -1;
+	if ((!no_built && check_builtins(name)) ||\
+		(ft_strchr(name, '/') && !access(name, F_OK))) 
+		return (name);
+	if (get_env(env, "PATH="))
+		dir = ft_strsplit(get_env(env, "PATH="), ':');
+	else
+		dir = NULL;
+	while (dir && dir[++i])
+	{
+		path = join_path(dir[i], name);
+		if (!access(path, F_OK))
+		{
+			lstat(path, &buf);
+			if ((buf.st_mode & S_IXUSR) && !S_ISDIR(buf.st_mode))
+				return (path);
+		}
+		free(path);
+	}
+	return (NULL);
+}
+
+/*si les fd different de ceux par default on remplace le fd par celui fournit
+ et close ces fd dans le pere avec les meme condition*/
+int 	exe_com(char *name, char **arg, char **env, int fd[3], t_list *toclose)
+{
+	pid_t pid;
+
+	if (name[0] != '/' && name[0] != '.')//verif passé sans path de fichier
+		return (exec_builtins(name, arg, env));//donc builtin
+	else
+	{
+		pid = fork();
+		if (pid < 0)
+			ft_putstr("fork fail\n");
+		else if (pid == 0)
+		{
+			multiclose(toclose);
+			if (fd[IN] != STDIN_FILENO)
+			{
+				dup2(fd[IN], STDIN_FILENO);
+				close(fd[IN]);
+			}
+			if (fd[OUT] != STDOUT_FILENO)
+			{
+				dup2(fd[OUT], STDOUT_FILENO);
+				close(fd[OUT]);
+			}
+			if (fd[ERR] != STDERR_FILENO)
+			{
+				dup2(fd[ERR], STDERR_FILENO);
+				close(fd[ERR]);
+			}
+			execve(name, arg, env);
+			ft_putstr("exec fail\n");
+		}
+		else
+		{
+			if (fd[IN] != STDIN_FILENO)
+				close(fd[IN]);
+			if (fd[OUT] != STDOUT_FILENO)
+				close(fd[OUT]);
+			if (fd[ERR] != STDERR_FILENO)
+				close(fd[ERR]);
+			return (1);
+		}
+	}
+	return(1);
+}
+
+//verifie la ligne de commande doit y avoir un pb avec les builins
+int 	verif_line(char *str, int fd[3], t_list *toclose)
+{
+	char **ltab;
+	char **env; //changer toute cette merde de env qui passe partout sg_env(NULL)
+
+	env = sg_env(NULL);
+	if (str[0])
+	{
+		ltab = ft_strsplit(str, ' ');
+		if (!(str = check(env, ltab[0], 0)))//check pour les builtin aussi
+		{
+			ft_putstr("Command not found\n"); //dit ca si ./trucexistepas, a regler dans check
+			return (-1);
+		}
+		else
+			return (exe_com(str, ltab, env, fd, toclose));
+	}
+	return (-1);
+}
